ced6b795ba3fa8ebacbe8c743226ac03b678e1b8
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-09-17 18:36:45 +0100

    Moved all icon image from App component to WeatherApp component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import "./App.css";
 import WeatherApp from "./components/WeatherApp/WeatherApp";
 
-// ---- Images ---- //
-import search_icon from "./components/Assets/search.png";
-import clear_icon from "./components/Assets/clear.png";
-import cloud_icon from "./components/Assets/cloud.png";
-import drizzle_icon from "./components/Assets/drizlle.png";
-import humidity_icon from "./components/Assets/humidity.png";
-import rain_icon from "./components/Assets/rain.png";
-import snow_Icon from "./components/Assets/snow.png";
-import wind_icon from "./components/Assets/wind.png";
-
 function App() {
   return (
     <div className="App">
--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import "./weatherApp.css";
 
+// ---- Images ---- //
+import search_icon from "../Assets/search.png";
+import clear_icon from "../Assets/clear.png";
+import cloud_icon from "../Assets/cloud.png";
+import drizzle_icon from "../Assets/drizlle.png";
+import humidity_icon from "../Assets/humidity.png";
+import rain_icon from "../Assets/rain.png";
+import snow_Icon from "../Assets/snow.png";
+import wind_icon from "../Assets/wind.png";
+
 const WeatherApp = () => {
   return <div>WeatherApp</div>;
 };
