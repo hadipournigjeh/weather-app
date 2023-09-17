ade65b59d0ed4437125fdde72e4d442ca012b875
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-09-17 18:27:51 +0100

    Added cloud icon image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import WeatherApp from "./components/WeatherApp/WeatherApp";
 // ---- Images ---- //
 import search_icon from "./components/Assets/search.png";
 import clear_icon from "./components/Assets/clear.png";
+import cloud_icon from "./components/Assets/cloud.png";
 
 function App() {
   return (
