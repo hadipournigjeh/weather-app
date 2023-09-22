2d21ff224c80837407be220536acded75dc7df27
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-09-22 19:43:40 +0100

    Updated img tag src to wicon

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -59,7 +59,7 @@ const WeatherApp = () => {
         </div>
       </div>
       <div className="weather-image">
-        <img src={cloud_icon} alt="cloud icon"></img>
+        <img src={wicon} alt="cloud icon"></img>
       </div>
       <div className="weather-temp">24°c</div>
       <div className="weather-location">London</div>
